90a76e476212a00d8425dbe8dbe0b1a152dca35e
Connor Cooper <cooper.connor.17@gmail.com>
2024-08-12 18:09:11 +1000

    whitespace

--- modules/template.nix
+++ modules/template.nix
@@ -1,10 +1,11 @@
+# TODO: https://nix.dev/guides/best-practices#with-scopes
 { pkgs, lib, config, ... }:
 
 with lib;
 let cfg = config.modules.PROGRAM;
 
 in {
-    options.modules.PROGRAM = { enable = mkEnableOption "PROGRAM"; };
-    config = mkIf cfg.enable {
-    };
+  options.modules.PROGRAM = { enable = mkEnableOption "PROGRAM"; };
+  config = mkIf cfg.enable {
+  };
 }
