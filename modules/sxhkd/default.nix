e60699915d5727f1e0a9043764ee1bdc5b035962
Connor Cooper <connor.cooper@proton.me>
2025-06-08 13:56:13 +1000

    Laptop buttons

--- modules/sxhkd/default.nix
+++ modules/sxhkd/default.nix
@@ -11,6 +11,12 @@ in {
       keybindings = {
         "XF86MonBrightnessUp" = "/run/current-system/sw/bin/light -A 10";
         "XF86MonBrightnessDown" = "/run/current-system/sw/bin/light -U 10";
+        "XF86AudioRaiseVolume" = "/run/current-system/sw/bin/pamixer -i 5; /run/current-system/sw/bin/dwm-msg run_command updatestatus_ipc";
+        "XF86AudioLowerVolume" = "/run/current-system/sw/bin/pamixer -d 5; /run/current-system/sw/bin/dwm-msg run_command updatestatus_ipc";
+        "XF86AudioMute" = "/run/current-system/sw/bin/pamixer -t; /run/current-system/sw/bin/dwm-msg run_command updatestatus_ipc";
+        "XF86AudioPrev" = "/run/current-system/sw/bin/playerctl previous";
+        "XF86AudioPlay" = "/run/current-system/sw/bin/playerctl play-pause";
+        "XF86AudioNext" = "/run/current-system/sw/bin/playerctl next";
       };
     };
     systemd.user.services.sxhkd = {
