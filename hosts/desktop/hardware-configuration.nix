8b8e42cbbbfffc8903547e531debbd2b180bcb21
Connor Cooper <cooper.connor.17@gmail.com>
2025-06-24 16:40:57 +1000

    Fix desktop config error

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -36,4 +36,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
 }
--- hosts/desktop/user.nix
+++ hosts/desktop/user.nix
@@ -1,29 +1,30 @@
-{ config, lib, inputs, ...}:
+{ config, lib, inputs, pkgs, ...}:
 
 {
     imports = [ ../../modules/default.nix ];
-    config.modules = {
-    #    # gui
-        firefox.enable = true;
-        shell.enable = true;
-    #    #foot.enable = true;
-    #    #eww.enable = true;
-    #    #dunst.enable = true;
-    #    #hyprland.enable = true;
-    #    #wofi.enable = true;
+    config = {
+        home.packages = [ pkgs.rehash ];
+        modules = {
+        #    # gui
+            firefox.enable = true;
+            shell.enable = true;
+        #    #foot.enable = true;
+        #    #eww.enable = true;
+        #    #dunst.enable = true;
+        #    #hyprland.enable = true;
+        #    #wofi.enable = true;
 
-    #    # cli
-    #    nvim.enable = true;
-    #    zsh.enable = true;
-    #    git.enable = true;
-    #    #gpg.enable = true;
-    #    #direnv.enable = true;
+        #    # cli
+        #    nvim.enable = true;
+        #    zsh.enable = true;
+        #    git.enable = true;
+        #    #gpg.enable = true;
+        #    #direnv.enable = true;
 
-    #    # system
-    #    #xdg.enable = true;
-    #    #packages.enable = true;
+        #    # system
+        #    #xdg.enable = true;
+        #    #packages.enable = true;
+        };
+        # Proprietary drivers
     };
-    # Proprietary drivers
-    services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia.open = true;
 }
